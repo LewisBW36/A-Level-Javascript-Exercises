7ea2150c059cb8571bddf156ebd6d5c751a83e0a
LewisBW36 <628lb14@canonslade.tbft.uk>
2026-09-11 09:12:33 +0000

    starting 2

--- Class Exercises/1 - Introduction to Javascript/Task 1c - Circles Calculations/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 1c - Circles Calculations/script.js
@@ -4,9 +4,14 @@ document.getElementById('calculateBtn').addEventListener('click', calculateCircl
 function calculateCircle() {
     // TODO:
     // 1. Get the radius value from the input field
+    let radius = parseFloat(document.getElementById('radius').value);
     // 2. Calculate:
     //    - Circumference using the formula: 2 * π * radius
+    const circumference = 2 * Math.PI * radius;
     //    - Area using the formula: π * radius^2
+    const area = Math.PI * (radius**2);
     // 3. Display both results in their respective span elements
     // Note: Use Math.PI for the value of π
+    document.getElementById('circumference').textContent = `${circumference}`;
+    document.getElementById('area').textContent = `${area}`
 }
--- Class Exercises/1 - Introduction to Javascript/Task 2a - Mark Percentage/script.js
+++ Class Exercises/1 - Introduction to Javascript/Task 2a - Mark Percentage/script.js
@@ -4,6 +4,10 @@ document.getElementById('calculateBtn').addEventListener('click', calculatePerce
 function calculatePercentage() {
     // TODO:
     // 1. Get the maximum mark and student's mark from the input fields
+    let maxMark = parseFloat(document.getElementById('maxMark').value);
+    let studentMark = parseFloat(document.getElementById('studentMark').value);
     // 2. Calculate the percentage using the formula: (student's mark ÷ maximum mark) × 100
+    const percentage = (studentMark / maxMark) * 100;
     // 3. Display the result in the percentage span element
+    document.getElementById('percentage').textContent = `${percentage}`
 }
