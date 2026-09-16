d8db2a82a72fbb3ede13050f8bbb1f4161ea72ec
LewisBW36 <628lb14@canonslade.tbft.uk>
2026-09-16 09:40:43 +0000

    2a dont wanna work do it l8r

--- Class Exercises/2 - Javascript Selection/Task 1d - Whats the Number/script.js
+++ Class Exercises/2 - Javascript Selection/Task 1d - Whats the Number/script.js
@@ -10,8 +10,16 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to check if a number is positive, negative, or zero
 function checkNumber() {
     // TODO: Get the number from the input field
-    
+    let number = parseFloat(document.getElementById('number').value);
     // TODO: Check if the number is positive, negative, or zero
-    
+    let result = ''
+    if (number > 0) {
+        result = "positive";
+    } else if (number < 0) {
+        result = "negative";
+    } else {
+        result = "zero";
+    }
     // TODO: Display the appropriate message (POSITIVE, NEGATIVE, or ZERO)
+    document.getElementById('result').textContent = `The number is ${result}`
 }
--- Class Exercises/2 - Javascript Selection/Task 2a - A Level Grade/script.js
+++ Class Exercises/2 - Javascript Selection/Task 2a - A Level Grade/script.js
@@ -10,18 +10,47 @@ document.addEventListener('DOMContentLoaded', function() {
 // Function to calculate the A-Level grade
 function calculateGrade() {
     // TODO: Get the marks for all four components
-    
+    let comp1;
+    let comp2;
+    let comp3;
+    let comp4;
+    comp1 = parseInt(document.getElementById('comp1').value);
+    comp2 = parseInt(document.getElementById('comp2').value);
+    comp3 = parseInt(document.getElementById('comp3').value);
+    comp4 = parseInt(document.getElementById('comp1').value);
     // TODO: Calculate the total marks
-    
+    let totalmarks;
+    totalmarks = comp1 + comp2 + comp3 + comp4
     // TODO: Calculate the percentage (total possible marks is 400)
-    
+    let percentage;
+    percentage = (totalmarks / 400) * 100
+    percentage = Math.round(percentage)
     // TODO: Determine the grade based on the percentage
+    let grade;
     // A: 80% or greater
+    if (percentage >= 80) {
+        grade = "A for.. amazing! Great job!!";
+    }
     // B: 70% or greater
+    else if (grade >= 70 && grade < 80) {
+        grade = "you should.. B happy with this grade!";
+    }
     // C: 60% or greater
+    else if (grade >= 60 && grade > 70) {
+        grade = "i C.. that you tried!";
+    }
     // D: 50% or greater
+    else if (grade >= 50 && grade > 60) {
+        grade = "a D is still ok..";
+    }
     // E: 40% or greater
+    else if (grade >= 40 && grade < 50) {
+        grade = "E.. for.. effort?";
+    }
     // U: Less than 40%
-    
+    else if ( percentage < 40 ) {
+        grade = "you got a U.. yikes";
+    }
     // TODO: Display the percentage and grade
+    document.getElementById('result').textContent = `Your percentage was ${percentage}%, ${grade}`
 }
